fe01dc97b3bfb81e36971fbbc095971f94357536
RutuBhoyar <rutujabhoyar3101@gmail.com>
2023-11-26 17:01:01 +0530

    Changes

--- Frontend/ecowastexchange/src/Components/Dashboard/Seller-Dashboard/Seller_Settings.js
+++ Frontend/ecowastexchange/src/Components/Dashboard/Seller-Dashboard/Seller_Settings.js
@@ -3,22 +3,70 @@ import {useState, useEffect} from "react";
 import seller from "../../../Images/seller.jpg";
 import "../../../Styles/Seller_Settings.css";
 import Seller_Navbar from "./Seller_Navbar";
+
 const Seller_Settings = () => {
 
     
-    const [formData, setFormData] = useState({
-        Name: '',
-        Phone: '',
-        Address: '',
-        Email: '',
-        City: '',
-        State: '',
-        Avatar: null,
-      });
+  const [formData, setFormData] = useState({
+    Name: '',
+    Phone: '',
+    Address: '',
+    Email: '',
+    City: '',
+    State: '',
+    Avatar: null,
+  });
 
-      const [editMode, setEditMode] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
-      const postData= async()=>{
+  useEffect(()=> {
+
+    const fetchUser = async() => {
+
+
+      const token = localStorage.getItem("auth-token")
+      
+      const res=await fetch('/api/seller/profile',
+      {
+        method:"POST",
+        headers:
+        {
+        "Content-Type":"application/json",
+        },
+        body: JSON.stringify({
+          auth_token: token
+        })
+      })
+
+        const data=await res.json()
+        console.log(data)
+        if(res.status===200)
+        {
+          
+          setFormData({
+
+              Name: data.data.Name,
+              Phone: data.data.Phone,
+              Address: data.data.Address,
+              Email: data.data.Email,
+              City: data.data.City,
+              State: data.data.State,
+              Password: data.data.Password,
+              Avatar: data.data.Avatar,
+              Id: data.data._id
+
+          })
+
+
+        }
+    }
+    
+    fetchUser()
+
+  }, [])
+
+  const postData= async()=>{
+    console.log("Hello");
               const token =localStorage.getItem('auth-token')
               const {Name,Email,Phone,Address,City,State,Avatar}=formData;
     
@@ -33,106 +81,69 @@ const Seller_Settings = () => {
               })
             })
     
-            const data= await res.json();
+            const data= await res.json()
     
             if(res.status===200)
             window.alert(data.message)
             else
             window.alert(data.error)
-            
+}
+
+
+
+
+  
+   
+  
+  const handleEditProfile = () => {
+    if(!editMode){
+    setEditMode(!editMode);
     }
+    else{
+      postData(formData.Id);
+      setEditMode(!editMode);
+
+    }
+  };
+
+
+  const postDetails=(pics)=>{
+
+    if(pics.type==='image/jpg'||pics.type==='image/png'||pics.type==='image/jpeg'||pics.type==='image/JPG'||pics.type==='image/PNG'||pics.type==='image/JPEG')
+    {
+      const data=new FormData();
+      data.append('file',pics);
+      data.append('upload_preset','Ecowastemanagement')
+      data.append('cloud_name','dfjwwbdv6')
+      fetch('https://api.cloudinary.com/v1_1/dfjwwbdv6/image/upload',{
+        method:"post",
+        body:data,
+
+      }).then((res)=>res.json()).then((data)=>{
+        setFormData({...formData,Avatar:data.url.toString()})
+      }).catch((err)=>{
+      })
+    }
+  
+  }
+
+  
+  const handleInputChange = (e) => {
     
-    
-    
-      useEffect(()=> {
-
-        const fetchUser = async() => {
-
-
-          const token = localStorage.getItem("auth-token")
-          const res=await fetch('/api/seller/profile',
-          {
-            method:"POST",
-            headers:
-            {
-            "Content-Type":"application/json",
-            },
-            body: JSON.stringify({
-              auth_token: token
-            })
-          })
-
-            const data=await res.json()
-            if(res.status===200)
-            {
-              
-              setFormData({
-
-                  Name: data.data.Name,
-                  Phone: data.data.Phone,
-                  Address: data.data.Address,
-                  Email: data.data.Email,
-                  City: data.data.City,
-                  State: data.data.State,
-                  Avatar: data.data.Avatar,
-
-
-              })
-
-
-            }
-        }
-        
-        fetchUser()
-
-      }, [])
-
-      const handleEditProfile = () => {
-        if(!editMode){
-        setEditMode(!editMode);
-        }
-        else{
-          postData();
-          setEditMode(!editMode);
-        }
-      };
-
-      const postDetails=(pics)=>{
-
-        if(pics.type==='image/jpg'||pics.type==='image/png'||pics.type==='image/jpeg'||pics.type==='image/JPG'||pics.type==='image/PNG'||pics.type==='image/JPEG')
-        {
-          const data=new FormData();
-          data.append('file',pics);
-          data.append('upload_preset','Ecowastemanagement')
-          data.append('cloud_name','dfjwwbdv6')
-          fetch('https://api.cloudinary.com/v1_1/dfjwwbdv6/image/upload',{
-            method:"post",
-            body:data,
-    
-          }).then((res)=>res.json()).then((data)=>{
-            setFormData({...formData,Avatar:data.url.toString()})
-          }).catch((err)=>{
-          })
-        }
+    const { name, value } = e.target;
       
-      }
-
-      const handleInputChange = (e) => {
-        const { name, value } = e.target;
         setFormData({
           ...formData,
           [name]: value,
-           
-        });
-      };
-
+    });
+  };
     return (
-        <div>
-      <Seller_Navbar></Seller_Navbar>
 
+        <div >
+<Seller_Navbar></Seller_Navbar>
       <div className="sellersetting-container">
         <div className="photo">
-          <label htmlFor="avatarInput">
+        <label htmlFor="avatarInput">
             <img src={formData.Avatar} alt="" />
             <input
               type="file"
@@ -148,6 +159,7 @@ const Seller_Settings = () => {
             onClick={handleEditProfile}
           >
             {editMode ? "Save" : "Edit profile"}
+            
           </button>
         </div>
 
@@ -228,7 +240,7 @@ const Seller_Settings = () => {
         </div>
       </div>
     </div>
-    )
+  );
 }
 
 export default Seller_Settings;
