d375abd7b164df0a359fc6bae880d33b72f6e5dc
RutuBhoyar <rutujabhoyar3101@gmail.com>
2023-11-27 12:23:00 +0530

    Backend company history and cart changes

---
+++ Backend/routes/deleteCompanyHistory.js
@@ -0,0 +1,55 @@
+const express = require('express')
+
+const Product = require('../models/productModel')
+const router = express.Router()
+
+
+router.post('/', async(req, res) => {
+
+    
+    const product_id =req.body.product_id;
+    console.log(product_id)
+    try
+    {
+        details=await Product.findById({_id:product_id})
+        
+
+    }
+    catch(err)
+    {
+        res.status(400).json({error:err.message})
+    }
+
+    if(details){
+    if(details.user_id=="" )
+    {
+        try{     
+            const product_data=await Product.findOneAndDelete({_id: product_id})
+
+            res.status(200).json({message: "Product removed from history"})
+    
+        }
+        catch(err) {
+    
+            res.status(400).json({error: err.message})
+        }
+    
+
+    }
+    else{
+        try{     
+            const product_data=await Product.findOneAndDelete({_id: product_id},{vendor_id:""})
+
+            res.status(200).json({message: "Product removed from history"})
+    
+        }
+        catch(err) {
+    
+            res.status(400).json({error: err.message})
+        }
+    
+
+    }
+    }
+})
+    module.exports = router 
